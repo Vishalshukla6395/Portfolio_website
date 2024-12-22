c8b2c13483c2100539852f7632362e3a16d99fbc
vishal shukla <you@example.com>
2024-12-22 10:39:16 +0530

    Update icons

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
   const header = document.querySelector("header");
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
-const navigationItems = document.querySelectorAll(".navigation a")
+const navigationItems = document.querySelectorAll(".navigation a");
 
-menuBtn.addEventListener("click",  () => {
+menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navigation.classList.toggle("active");
 });
@@ -21,13 +21,13 @@ navigationItems.forEach((navigationItem) => {
 
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
   scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
 
 scrollBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
 window.addEventListener("scroll", reveal);
